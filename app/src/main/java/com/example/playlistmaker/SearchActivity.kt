104bfe95444635edf3974d53d5f16a092faf45e3
Виктор Бляхеров <viktor.blyakherov@yandex.ru>
2023-07-04 08:40:27 +0300

    Sprint 9. Review

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,34 +1,43 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
 
-    var searchText: String = ""
+    private var searchText: String = ""
+    private lateinit var inputEditText: EditText
+    private lateinit var buttonBack: ImageButton
+    private lateinit var clearButton: ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        inputEditText = findViewById<EditText>(R.id.searchEditText)
 
-        val buttonBack = findViewById<ImageButton>(R.id.backSearch)
+        buttonBack = findViewById<ImageButton>(R.id.backSearch)
 
         buttonBack.setOnClickListener {
             this.finish()
         }
 
 
-        val inputEditText = findViewById<EditText>(R.id.searchEditText)
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        clearButton = findViewById<ImageView>(R.id.clearIcon)
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -49,10 +58,6 @@ class SearchActivity : AppCompatActivity() {
 
     }
 
-    companion object {
-        const val SEARCH_TEXT = "SEARCH_TEXT"
-    }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(SEARCH_TEXT,searchText)
@@ -60,7 +65,6 @@ class SearchActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val inputEditText = findViewById<EditText>(R.id.searchEditText)
         searchText = savedInstanceState.getString(SEARCH_TEXT,"")
         inputEditText.setText(searchText)
     }
@@ -72,4 +76,8 @@ class SearchActivity : AppCompatActivity() {
             View.VISIBLE
         }
     }
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+
 }
